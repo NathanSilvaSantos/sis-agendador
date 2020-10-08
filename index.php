fd555ac6dc674a1bb617ea80a15fa3de07568670
NathanSilvaSantos <nathan013silva@gmail.com>
2020-10-07 22:32:04 -0300

    criando navegação dinâmica

--- index.php
+++ index.php
@@ -9,15 +9,40 @@
     <header>
         <h1>Sistema Agendador 1.0</h1>
         <nav>
-            <a href="#">Home</a> |
-            <a href="#">Contato</a> |
-            <a href="#">Tarefas</a> |
-            <a href="#">Eventos</a>
+            <a href="index.php?menuop=home">Home</a> |
+            <a href="index.php?menuop=contatos">Contatos</a> |
+            <a href="index.php?menuop=tarefas">Tarefas</a> |
+            <a href="index.php?menuop=eventos">Eventos</a>
         </nav>
     </header>
 
     <main>
-        <?php ?>
+        <?php
+            $menuop = isset($_GET["menuop"])?$_GET["menuop"]:"home";
+
+            switch ($menuop) {
+                case 'home':
+                    include("paginas/home/home.php");
+                    break;
+                
+                case 'contatos':
+                    include("paginas/contatos/contatos.php");
+                    break;
+
+                case 'tarefas':
+                    include("paginas/tarefas/tarefas.php");
+                    break;
+
+                case 'eventos':
+                    include("paginas/eventos/eventos.php");
+                    break;
+                    
+                    
+                default:
+                    include("paginas/home/home.php");
+                    break;
+            }
+        ?>
     </main>
 </body>
 </html>
